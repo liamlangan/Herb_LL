99638eeed8f7dc343ac55cacef347d321d2c0963
liamlangan <llangan@tcd.ie>
2018-05-29 21:50:34 +0200

    initial relationships

---
+++ Herbivory_trade_offs.R
@@ -0,0 +1,13 @@
+# Author: Liam Langan
+# Herbivory trade-offs
+
+mass <- seq(1, 5000, by=1)
+daily_food_intake <- function(mass){0.695*(mass^-0.236)} # Fig. 5.1 Normal Owen Smith
+plot(mass, daily_food_intake(mass), ylab="Daily food intake (% of body mass)", xlab="Body mass (kg)", ylim=c(0,1))
+
+daily_food_intake2 <- function(mass){0.6*(mass^-0.191)} # Fig. 5.2 Normal Owen Smith
+# plot(mass, daily_food_intake2(mass), ylab="Daily food intake (% of body mass)", xlab="Body mass (kg)", ylim=c(0,1))
+lines(mass, daily_food_intake2(mass), col="red", lwd=3)
+
+mean_retention_time_hindgut <- function(mass){32*(mass^0.135)}
+plot(mass, mean_retention_time_hindgut(mass), ylab="Mean retention time (h)", xlab="Body mass (kg)", ylim=c(0,120))
