3eb6cac0b396c5c89ef52f149059fb370d7bb2b9
liamlangan <llangan@tcd.ie>
2018-05-29 22:03:00 +0200

    taking a break

--- Herbivory_trade_offs.R
+++ Herbivory_trade_offs.R
@@ -11,3 +11,8 @@ lines(mass, daily_food_intake2(mass), col="red", lwd=3)
 
 mean_retention_time_hindgut <- function(mass){32*(mass^0.135)}
 plot(mass, mean_retention_time_hindgut(mass), ylab="Mean retention time (h)", xlab="Body mass (kg)", ylim=c(0,120))
+
+
+cell_wall_digestion_all_species <- function(mean_retention_time_hindgut){ (4.95 - (0.034*mean_retention_time_hindgut))}
+plot(mean_retention_time_hindgut(mass), log(cell_wall_digestion_all_species(mean_retention_time_hindgut(mass))))
+# not working
